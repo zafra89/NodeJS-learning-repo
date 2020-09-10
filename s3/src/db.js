2df0e29fe642189b6b050a20af619e422e638fde
Jose Luis García Zafra <51385853+zafra89@users.noreply.github.com>
2020-09-10 23:46:22 +0200

    Update db.js

--- s3/src/db.js
+++ s3/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // URL local de nuestra base de datos en mongoose y su nombre upgrade_class_3
-const DB_URL = 'mongodb+srv://zafra89:30Madeinjapan1089@cluster0.crgea.mongodb.net/mongoosepruebas?retryWrites=true&w=majority';
+const DB_URL = 'mongodb+srv://zafra89:<PASSWORD>@cluster0.crgea.mongodb.net/mongoosepruebas?retryWrites=true&w=majority';
 
 // Función que conecta nuestro servidor a la base de datos de MongoDB mediante mongoose
 mongoose.connect(DB_URL, {
